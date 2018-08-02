4c6d71821eccb642fca4e583628ad387e3b7a131
Igor Djuric <igor.djuric011@gmail.com>
2018-08-02 18:04:59 +0200

    Cart page

--- templates/template-parts/category-page/gf-category-with-filters.php
+++ templates/template-parts/category-page/gf-category-with-filters.php
@@ -82,7 +82,7 @@
                  * @hooked WC_Structured_Data::generate_product_data() - 10
                  */
                 ?>
-                <div class="col-12"><?php do_action( 'woocommerce_shop_loop' )?></div>
+                <?php do_action( 'woocommerce_shop_loop' )?>
                 <?php
                 wc_get_template_part( 'content', 'product' );
             }
