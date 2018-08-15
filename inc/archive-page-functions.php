70cc6423c2e725193bd72b0aaa90854293817f79
Igor Djuric <igor.djuric011@gmail.com>
2018-08-15 14:14:46 +0200

    Pagination fixed, archive page before and after shop loop items fixed

--- inc/archive-page-functions.php
+++ inc/archive-page-functions.php
@@ -115,6 +115,10 @@ function gf_display_categories_on_archive_page()
 }
 remove_action( 'woocommerce_before_shop_loop', 'wc_print_notices', 10 );
 remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
+remove_action( 'woocommerce_after_shop_loop', 'woocommerce_pagination', 10 );
+
 add_action('woocommerce_before_shop_loop','woocommerce_result_count',20);
-add_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 24 );
-add_action( 'woocommerce_before_shop_loop', 'woocommerce_pagination', 26 );
+add_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 26);
+add_action( 'woocommerce_after_shop_loop', 'woocommerce_catalog_ordering', 26 );
+add_action( 'woocommerce_before_shop_loop', 'woocommerce_pagination', 27 );
+add_action( 'woocommerce_after_shop_loop', 'woocommerce_pagination', 27 );
