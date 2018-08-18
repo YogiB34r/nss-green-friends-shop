b13e7df10441a298b8f2a418c32e500f32f03388
Mladen <mladen.sijan@live.com>
2018-08-18 11:59:23 +0200

    Search input on mobile put below navbar to be static

--- searchform.php
+++ searchform.php
@@ -3,12 +3,3 @@
   <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', '' ) ?>" value="<?php echo get_search_query() ?>" name="s" />
   <button type="submit" class="search-submit"><i class="fa fa-search"></i></button>
 </form>
-
-<form role="search" method="get" class="gf-search-form gf-search-form--mobile" action="<?php echo esc_url( get_permalink( wc_get_page_id( 'shop' ) )) ?>">
-  <!-- <div class="search-toggle-wrapper"><div class="gf-search-toggle"><i class="fa fa-search"></i></div></div> -->
-  <span class="screen-reader-text"><?php _x( 'Search for:', 'label' )?></span>
-  <div class="search-input-wrapper">
-    <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', '' ) ?>" value="<?php echo get_search_query() ?>" name="s" />
-    <button type="submit" class="search-submit"><i class="fa fa-search"></i></button>
-  </div>
-</form>
