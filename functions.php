26829f7bb0bd74f0a1d06f014f88b47d50561590
djavolak <djavolak@mail.ru>
2018-08-22 22:57:43 +0200

    listing out of stock items last

--- functions.php
+++ functions.php
@@ -34,7 +34,7 @@ add_filter('posts_clauses', 'order_by_stock_status');
 function order_by_stock_status($posts_clauses) {
     global $wpdb;
     // only change query on WooCommerce loops
-    if (is_woocommerce() && (is_shop() || is_product_category() || is_product_tag() || is_product_taxonomy())) {
+    if (is_woocommerce() && (is_shop() || is_product_category() || is_product_tag())) {
         $posts_clauses['join'] .= " INNER JOIN $wpdb->postmeta istockstatus ON ($wpdb->posts.ID = istockstatus.post_id) ";
         $posts_clauses['orderby'] = " istockstatus.meta_value ASC, " . $posts_clauses['orderby'];
         $posts_clauses['where'] = " AND istockstatus.meta_key = '_stock_status' AND istockstatus.meta_value <> '' " . $posts_clauses['where'];
