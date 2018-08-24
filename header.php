246336e5d13890667b884ca3d10beddaf91fe700
Mladen <mladen.sijan@live.com>
2018-08-24 12:31:59 +0200

    Style fixes
    Related products box fixed,
    search input width fixed,...

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
   <div id="page" class="hfeed site">
     <header id="masthead" class="site-header" role="banner">
       <div class="container-fluid container--navigation">
-        <div class="row gf-top-bar">
+        <div class="gf-top-bar">
           <div class="row gf-top-bar__container">
             <div class="col-3"></div>
             <div class="col-9 gf-top-bar__menu">
