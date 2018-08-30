07836798dc72d62daf5ce01baf3037895a41cc20
Mladen <mladen.sijan@live.com>
2018-08-30 16:35:08 +0200

    Added lines between products in loop, added radio-search for mobile

--- header.php
+++ header.php
@@ -49,6 +49,25 @@
         <button type="submit" class="search-submit"><i class="fa fa-search"></i></button>
       </div>
     </form>
+    <div class="gf-radio-search-wrapper gf-radio-search-wrapper--mobile">
+      <?php if (get_queried_object() && is_product_category()): ?>
+          <input class="search-radio-box" type="radio" name="search-radiobutton" checked="checked" value="category">
+          <label for="search-checkbox"><?= get_queried_object()->name ?></label>
+          <input class="search-radio-box" type="radio" name="search-radiobutton" value="shop">
+          <label for="search-checkbox">Pretraga celog sajta</label>
+      <?php endif ;?>
+    </div>
+    <script type="text/javascript">
+        jQuery(document).ready(function($){
+            var radioValue='';
+            $(".gf-search-form--mobile").submit(function () {
+                var radioValue = $('.gf-search-form--mobile input[name=search-radiobutton]:checked').val();
+                if (radioValue === 'category'){
+                    $(".gf-search-form").attr("action", "");
+                }
+            });
+        });
+    </script>
     <div id="content" class="site-content" tabindex="-1">
       <div class="col-full">
         <div class="gf-main-content-container">
--- searchform.php
+++ searchform.php
@@ -6,12 +6,14 @@
            value="<?= get_search_query() ?>" name="s"/>
     <button type="submit" class="search-submit"><i class="fa fa-search"></i></button>
 </form>
-<?php if (get_queried_object() && is_product_category()): ?>
-    <input class="search-radio-box" type="radio" name="search-radiobutton" checked="checked" value="category">
-    <label for="search-checkbox"><?= get_queried_object()->name ?></label>
-    <input class="search-radio-box" type="radio" name="search-radiobutton" value="shop">
-	<label for="search-checkbox">Pretraga celog sajta</label>
-<?php endif ;?>
+<div class="gf-radio-search-wrapper">
+  <?php if (get_queried_object() && is_product_category()): ?>
+      <input class="search-radio-box" type="radio" name="search-radiobutton" checked="checked" value="category">
+      <label for="search-checkbox"><?= get_queried_object()->name ?></label>
+      <input class="search-radio-box" type="radio" name="search-radiobutton" value="shop">
+    	<label for="search-checkbox">Pretraga celog sajta</label>
+  <?php endif ;?>
+</div>
 <script type="text/javascript">
     jQuery(document).ready(function($){
         var radioValue='';
