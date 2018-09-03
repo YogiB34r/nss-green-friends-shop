a6292fcc0ccab3154f47828c1e87db52475d5d31
Apache <apache@nss-devel-1.m.ha.rs>
2018-09-03 15:15:02 +0200

    replaced wc core function thatt appears to not work

--- redirect-old-products.php
+++ redirect-old-products.php
@@ -2,9 +2,21 @@
 /* Template Name: redirectOldProducts */
 
 $sku = $_GET['id'];
-$id = wc_get_product_id_by_sku($id);
-$url = get_permalink($id);
+$product = get_product_by_sku($sku);
+$url = get_permalink($product->get_id());
+//echo $id = wc_get_product_id_by_sku($id);
+//echo $url = get_permalink($id);
 
 header("HTTP/1.1 301 Moved Permanently");
 header('Location: '.  $url);
 
+function get_product_by_sku( $sku ) {
+    global $wpdb;
+
+    $product_id = $wpdb->get_var( $wpdb->prepare( "SELECT post_id FROM $wpdb->postmeta WHERE meta_key='_sku' AND meta_value='%s' LIMIT 1", $sku ) );
+    if ($product_id){
+	 return new WC_Product( $product_id );
+    }
+
+    return null;
+}
