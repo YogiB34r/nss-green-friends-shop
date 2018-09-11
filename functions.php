299745355ee9b2bba776518d90426756f5b713f8
Vladimir Jankovic <v.jankovic992@gmail.com>
2018-09-11 06:05:16 +0200

    added function for inserting into array by index and checking multidimenisonal array diffrence

--- functions.php
+++ functions.php
@@ -46,30 +46,33 @@ function change_existing_currency_symbol($currency_symbol, $currency)
 //    }
 //    return $posts_clauses;
 //}
-function gf_get_categories($exlcude=array())
+function gf_get_categories($exlcude = array())
 {
     $args = array(
         'orderby' => 'name',
         'order' => 'asc',
         'hide_empty' => false,
-        'exclude'=> $exlcude,
+        'exclude' => $exlcude,
     );
     $product_cats = get_terms('product_cat', $args);
     return $product_cats;
 }
-function gf_get_top_level_categories($exclude=array()){
-    $top_level_categories =[];
+
+function gf_get_top_level_categories($exclude = array())
+{
+    $top_level_categories = [];
     $param = '';
-    if (isset($exclude)){
+    if (isset($exclude)) {
         $param = $exclude;
     }
-    foreach (gf_get_categories($param) as $category){
-        if (!$category->parent){
-            $top_level_categories[]= $category;
+    foreach (gf_get_categories($param) as $category) {
+        if (!$category->parent) {
+            $top_level_categories[] = $category;
         }
     }
     return $top_level_categories;
 }
+
 function gf_get_second_level_categories($parent_id = null)
 {
     $categories = gf_get_categories();
@@ -85,16 +88,18 @@ function gf_get_second_level_categories($parent_id = null)
             if ($category->parent == $parent_id) {
                 $second_level_categories[] = $category;
             }
-        }elseif (in_array($category->parent, $top_level_ids)) {
+        } elseif (in_array($category->parent, $top_level_ids)) {
             $second_level_categories[] = $category;
         }
     }
     return $second_level_categories;
 }
-function gf_get_third_level_categories($parent_id = null){
+
+function gf_get_third_level_categories($parent_id = null)
+{
     $categories = gf_get_categories();
     $second_level_ids = [];
-    foreach (gf_get_second_level_categories() as $cat){
+    foreach (gf_get_second_level_categories() as $cat) {
         $second_level_ids[] = $cat->term_id;
     }
     $third_level_categories = [];
@@ -103,39 +108,97 @@ function gf_get_third_level_categories($parent_id = null){
             if ($category->parent == $parent_id) {
                 $third_level_categories[] = $category;
             }
-        }elseif (in_array($category->parent, $second_level_ids)) {
+        } elseif (in_array($category->parent, $second_level_ids)) {
             $third_level_categories[] = $category;
         }
     }
     return $third_level_categories;
 }
-function gf_check_level_of_category($cat_id){
+//Testira razliku array-a order sensitive
+function gf_array_reccursive_difrence(array $array1, array $array2, array $_ = null)
+{
+    $diff = [];
+    $args = array_slice(func_get_args(), 1);
+    foreach ($array1 as $key => $value) {
+        foreach ($args as $item) {
+            if (is_array($item)) {
+                if (array_key_exists($key, $item)) {
+                    if (is_array($value) && is_array($item[$key])) {
+                        $tmpDiff = gf_array_reccursive_difrence($value, $item[$key]);
+
+                        if (!empty($tmpDiff)) {
+                            foreach ($tmpDiff as $tmpKey => $tmpValue) {
+                                if (isset($item[$key][$tmpKey])) {
+                                    if (is_array($value[$tmpKey]) && is_array($item[$key][$tmpKey])) {
+                                        $newDiff = array_diff($value[$tmpKey], $item[$key][$tmpKey]);
+                                    } else if ($value[$tmpKey] !== $item[$key][$tmpKey]) {
+                                        $newDiff = $value[$tmpKey];
+                                    }
+
+                                    if (isset($newDiff)) {
+                                        $diff[$key][$tmpKey] = $newDiff;
+                                    }
+                                } else {
+                                    $diff[$key][$tmpKey] = $tmpDiff;
+                                }
+                            }
+                        }
+                    } else if ($value !== $item[$key]) {
+                        $diff[$key] = $value;
+
+                    }
+                } else {
+                    $diff[$key] = $value;
+                }
+            }
+        }
+    }
+
+    return $diff;
+}
+
+function gf_insert_in_array_by_index($array, $index, $val)
+{
+    $size = count($array); //because I am going to use this more than one time
+    if (!is_int($index) || $index < 0 || $index > $size) {
+        return -1;
+    } else {
+        $temp = array_slice($array, 0, $index);
+        $temp[] = $val;
+        return array_merge($temp, array_slice($array, $index, $size));
+    }
+}
+
+function gf_check_level_of_category($cat_id)
+{
     $result = null;
-    $top_level_ids=[];
-    $second_level_ids=[];
-    $third_level_ids=[];
-    foreach (gf_get_top_level_categories() as $category){
-        $top_level_ids[]=$category->term_id;
+    $top_level_ids = [];
+    $second_level_ids = [];
+    $third_level_ids = [];
+    foreach (gf_get_top_level_categories() as $category) {
+        $top_level_ids[] = $category->term_id;
     }
-    foreach (gf_get_second_level_categories() as $category){
-        $second_level_ids[]=$category->term_id;
+    foreach (gf_get_second_level_categories() as $category) {
+        $second_level_ids[] = $category->term_id;
     }
-    foreach (gf_get_third_level_categories() as $category){
-        $third_level_ids[]=$category->term_id;
+    foreach (gf_get_third_level_categories() as $category) {
+        $third_level_ids[] = $category->term_id;
     }
-    if (in_array($cat_id,$top_level_ids)){
+    if (in_array($cat_id, $top_level_ids)) {
         $result = 1;
     }
-    if (in_array($cat_id,$second_level_ids)){
+    if (in_array($cat_id, $second_level_ids)) {
         $result = 2;
     }
-    if (in_array($cat_id,$third_level_ids)){
+    if (in_array($cat_id, $third_level_ids)) {
         $result = 3;
     }
     return $result;
 }
+
 //add_filter('pre_get_posts', 'order_by_stock_status');
-function order_by_stock_status($posts_clauses) {
+function order_by_stock_status($posts_clauses)
+{
     global $wpdb;
 
     // only change query on WooCommerce loops
@@ -153,14 +216,16 @@ function order_by_stock_status($posts_clauses) {
  * @param $uploads
  * @return mixed
  */
-function upload_dir_filter($uploads){
+function upload_dir_filter($uploads)
+{
     //$day = date('d');
-	$day = date('d/i');
+    $day = date('d/i');
     $uploads['path'] .= '/' . $day;
-    $uploads['url']  .= '/' . $day;
+    $uploads['url'] .= '/' . $day;
 
     return $uploads;
 }
+
 add_filter('upload_dir', 'upload_dir_filter');
 
 //ne brisati ovo zatrebace mozda <3 Vlada
