a92187001d1b8e932fa67848c62808818566dc98
Vladimir Jankovic <v.jankovic992@gmail.com>
2018-09-14 22:43:42 +0200

    added all js and css files to theme

---
+++ assets/js/gf-admin.js
@@ -0,0 +1,201 @@
+jQuery(document).ready(function ($) {
+//===========
+// GF WIDGETS
+//===========//
+    var custom_uploader;
+
+    function clickHandler(event, input, submitButton, target) {
+        event.preventDefault();
+        //Extend the wp.media object
+        custom_uploader = wp.media.frames.file_frame = wp.media({
+            title: 'Upload image',
+            button: {
+                text: 'Select'
+            },
+            multiple: true
+        });
+        //When a file is selected, grab the URL and set it as the text field's value
+        custom_uploader.on('select', function () {
+            var attachment = custom_uploader.state().get('selection').first().toJSON();
+            input.val(attachment.url);
+            submitButton.click();
+        });
+
+        //If the uploader object has already been created, reopen the dialog
+        if (custom_uploader) {
+            custom_uploader.open();
+            return;
+        }
+
+        //Open the uploader dialog
+        custom_uploader.open();
+        return;
+    }
+
+//Gf- image slider
+    $('#gf-homepage-row-1').on('click', '.gf-upload-image-1', function (e) {
+        clickHandler(e, $('.image_1_value'), $('#widget-41_gf_image_slider_widget-5 input[name="savewidget"]'));
+    });
+    $('#gf-homepage-row-1').on('click', '.gf-upload-image-2', function (e) {
+        clickHandler(e, $('.image_2_value'), $('#widget-41_gf_image_slider_widget-5 input[name="savewidget"]'));
+    });
+    $('#gf-homepage-row-1').on('click', '.gf-upload-image-3', function (e) {
+        clickHandler(e, $('.image_3_value'), $('#widget-41_gf_image_slider_widget-5 input[name="savewidget"]'));
+    });
+    $('#gf-homepage-row-1').on('click', '#gf-remove-image-1', function () {
+        if (confirm("Da li ste sigurni da želite da obrišete sliku ?")) {
+            $('.image_1_value').val('');
+            $('#widget-41_gf_image_slider_widget-5 input[name="savewidget"]').click();
+        }
+    });
+    $('#gf-homepage-row-1').on('click', '#gf-remove-image-2', function () {
+        if (confirm("Da li ste sigurni da želite da obrišete sliku ?")) {
+            $('.image_2_value').val('');
+            $('#widget-41_gf_image_slider_widget-5 input[name="savewidget"]').click();
+        }
+    });
+    $('#gf-homepage-row-1').on('click', '#gf-remove-image-3', function () {
+        if (confirm("Da li ste sigurni da želite da obrišete sliku ?")) {
+            $('.image_3_value').val('');
+            $('#widget-41_gf_image_slider_widget-5 input[name="savewidget"]').click();
+        }
+    });
+
+
+//GF - image banners
+    $('#gf-homepage-row-1').on('click', '.gf-upload-banner-image-1', function (e) {
+        clickHandler(e, $('.image_banner_1_value'), $('#widget-gf_image_banners_widget-2-savewidget'));
+    });
+    $('#gf-homepage-row-1').on('click', '.gf-upload-banner-image-2', function (e) {
+        clickHandler(e, $('.image_banner_2_value'), $('#widget-gf_image_banners_widget-2-savewidget'));
+    });
+    $('#gf-homepage-row-1').on('click', '.gf-upload-banner-image-3', function (e) {
+        clickHandler(e, $('.image_banner_3_value'), $('#widget-gf_image_banners_widget-2-savewidget'));
+    });
+    $('#gf-homepage-row-1').on('click', '.gf-upload-banner-image-4', function (e) {
+        clickHandler(e, $('.image_banner_4_value'), $('#widget-gf_image_banners_widget-2-savewidget'));
+    });
+
+
+//GF- custom logo
+    $('#gf-header-row-2-col-1').on('click', '.gf-upload-image-logo', function (e) {
+        clickHandler(e, $('.logo-image-value'), $('#widget-gf_custom_logo_widget-3-savewidget'));
+    });
+
+//GF- product slider
+    $('#gf-homepage-row-2').on('change', '.gf-category-select', function () {
+        $('#widget-gf_product_slider_widget-12-savewidget').click();
+    });
+
+    /* show lightbox when clicking a thumbnail */
+    $('a.thumb').click(function (event) {
+        event.preventDefault();
+        var content = $('.modal-body');
+        content.empty();
+        var title = $(this).attr("title");
+        $('.modal-title').html(title);
+        content.html($(this).html());
+        $(".modal-profile").modal({
+            show: true
+        });
+    });
+//====================
+// GF PRODUCT STICKERS
+//====================//
+    var custom_uploader;
+
+    function clickHandler(event, input, submitButton, target) {
+        event.preventDefault();
+        //Extend the wp.media object
+        custom_uploader = wp.media.frames.file_frame = wp.media({
+            title: 'Upload image',
+            button: {
+                text: 'Select'
+            },
+            multiple: false
+        });
+        //When a file is selected, grab the URL and set it as the text field's value
+        custom_uploader.on('select', function() {
+            var attachment = custom_uploader.state().get('selection').first().toJSON();
+            input.val(attachment.url);
+            submitButton.click();
+        });
+
+        //If the uploader object has already been created, reopen the dialog
+        if (custom_uploader) {
+            custom_uploader.open();
+            return;
+        }
+
+        //Open the uploader dialog
+        custom_uploader.open();
+        return;
+    }
+
+    $('.row').on('click', '#upload-sticker-image-new', function(e) {
+        clickHandler(e, $('.image_select_new'), $('#sticker_submit'));
+    });
+    $('.row').on('click', '#upload-sticker-image-soldout', function(e) {
+        clickHandler(e, $('.image_select_soldout'), $('#sticker_submit'));
+    });
+    $('.row').on('click', '#upload-sticker-image-sale', function(e) {
+        clickHandler(e, $('.image_select_sale'), $('#sticker_submit'));
+    });
+
+//=======================
+// GF SORTABLE CATEGORIES
+//=======================//
+
+    /********************************************/
+    /* AJAX SAVE FORM */
+    /********************************************/
+    //ako dodje do nekog problema ovo treba da se koristi valjda :P
+    // $('#theme-options-form').submit(function () {
+    //     $(this).ajaxSubmit({
+    //         onLoading: $('.loader').show(),
+    //         success: function () {
+    //             $('.loader').hide();
+    //             $('#save-result').fadeIn();
+    //             setTimeout(function () {
+    //                 $('#save-result').fadeOut('fast');
+    //             }, 2000);
+    //         },
+    //         timeout: 5000
+    //     });
+    //     return false;
+    // });
+    $('.accordion-first-level').accordion({
+        collapsible: true,
+        header: ">h2",
+        heightStyle: "content",
+        active:false,
+        icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
+    });
+    $('.accordion-second-level').accordion({
+        collapsible: true,
+        header: ">h4",
+        heightStyle: "content",
+        active:false,
+        icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
+    });
+    $('.filter-fields-list').sortable({
+        handle: "h2",
+        axis: 'y',
+        cursor: 'move',
+        items: 'li',
+    });
+    $('.parent-cat-children').sortable({
+        handle: "h4",
+        axis: 'y',
+        cursor: 'move',
+        items: 'li',
+    });
+    $('.child-cat-children').sortable({
+        handle: "h5",
+        axis: 'y',
+        cursor: 'move',
+        items: 'li',
+    });
+
+
+});
--- assets/js/gf-front.js
+++ assets/js/gf-front.js
@@ -175,4 +175,89 @@ jQuery(document).ready(function () {
     //     data: filterContent
     //   });
     // });
+
+//===========
+// GF WIDGETS
+//===========//
+
+//GF PRODUCT SLIDER //
+//@todo din't work when included with evrything (find fix)
+    // if (typeof gfSliderColumnCount !== "undefined") {
+    //     //@Important activate slider after tab is displayed in order to have access to proper width
+    //     $("#tabs").tabs({
+    //         activate: function(event, ui) {
+    //             if (!$(ui.newPanel.selector).hasClass('slick-initialized')) {
+    //                 startSlider(ui.newPanel.selector);
+    //             }
+    //         }
+    //     });
+    //     startSlider('#tabs-0');
+    //     hookSliderEvents();
+    //     startSlider('.without-tabs');
+    // }
+    //
+    // function startSlider(selector) {
+    //     $(selector).slick({
+    //         infinite: true,
+    //         slidesToShow: gfSliderColumnCount,
+    //         slidesToScroll: gfSliderColumnCount,
+    //         arrows: false,
+    //         dots: false,
+    //         responsive: [{
+    //             breakpoint: 1024,
+    //             settings: {
+    //                 slidesToShow: 3,
+    //                 slidesToScroll: 3,
+    //                 infinite: true
+    //             }
+    //         },
+    //             {
+    //                 breakpoint: 600,
+    //                 settings: {
+    //                     slidesToShow: 2,
+    //                     slidesToScroll: 2
+    //                 }
+    //             },
+    //             {
+    //                 breakpoint: 480,
+    //                 settings: {
+    //                     slidesToShow: 1,
+    //                     slidesToScroll: 1
+    //                 }
+    //             }
+    //         ]
+    //     });
+    // }
+
+    function hookSliderEvents() {
+        // standard carousel controls
+        $('.widget_gf_product_slider_without_tabs_widget .product-slider__control-prev').click(function(e) {
+            e.preventDefault();
+            $(this).parents('.widget_gf_product_slider_without_tabs_widget').find('.slider-inner').slick('slickPrev');
+        });
+
+        $('.widget_gf_product_slider_without_tabs_widget .product-slider__control-next').click(function(e) {
+            e.preventDefault();
+            $(this).parents('.widget_gf_product_slider_without_tabs_widget').find('.slider-inner').slick('slickNext');
+        });
+
+        //tabbed carousel controls
+        $('#tabs .product-slider__control-prev').click(function(e) {
+            e.preventDefault();
+            $('.slider-inner').each(function(key, value) {
+                if ($(value).attr('aria-hidden') == 'false') {
+                    $(this).slick('slickPrev');
+                }
+            });
+        });
+
+        $('#tabs .product-slider__control-next').click(function(e) {
+            e.preventDefault();
+            $('.slider-inner').each(function(key, value) {
+                if ($(value).attr('aria-hidden') == 'false') {
+                    $(this).slick('slickNext');
+                }
+            });
+        });
+    }
 });
