94d5d1fcd70fe52788b039da563969360bcd7b7f
YogiB34r <v.jankovic992@gmail.com>
2018-09-24 20:38:15 +0200

    switched classes for searchform

--- searchform.php
+++ searchform.php
@@ -1,18 +1,18 @@
 <form role="search" method="get" class="gf-search-form"
       action="/pretraga/">
     <span class="screen-reader-text"><?php _x('Search for:', 'label') ?></span>
-    <input type="search" autocomplete="off" class="search-field" name="query"
+    <input type="search" autocomplete="off" class="search-field gf-search-box" name="query"
            placeholder="<?=esc_attr_x('Unesite frazu pretrage &hellip;', '')?>"
-           value="<?= get_search_query() ?>" id="gf-search-box" />
+           value="<?= get_search_query() ?>"/>
     <button type="submit" class="search-submit"><i class="fa fa-search"></i></button>
-    <div id="suggesstion-box" class="gf-autocomplete-results"></div>
+    <div class="gf-autocomplete-results suggesstion-box"></div>
 </form>
 <div class="gf-radio-search-wrapper">
   <?php if (get_queried_object() && is_product_category()): ?>
-      <input class="search-radio-box" type="radio" id="search-radiobutton-cat" name="search-radiobutton-cat" checked="checked" value="category">
-      <label for="search-radiobutton-cat"><?= get_queried_object()->name ?></label>
-      <input class="search-radio-box" type="radio" id="search-radiobutton-shop" name="search-radiobutton-shop" value="shop">
-    	<label for="search-radiobutton-shop">Pretraga celog sajta</label>
+      <input class="search-radio-box" type="radio" name="search-radiobutton" checked="checked" value="category">
+      <label for="search-checkbox"><?= get_queried_object()->name ?></label>
+      <input class="search-radio-box" type="radio" name="search-radiobutton" value="shop">
+    	<label for="search-checkbox">Pretraga celog sajta</label>
   <?php endif ;?>
 </div>
 <script type="text/javascript">
