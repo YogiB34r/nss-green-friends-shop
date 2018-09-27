725f0fa688926ff339666819b6fc6ce5c4e8e597
Igor Djuric <igor.djuric011@gmail.com>
2018-09-27 13:08:31 +0200

    text changed and removed search form on 404 page

--- 404.php
+++ 404.php
@@ -12,26 +12,25 @@
 
 get_header(); ?>
 
-<div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+    <div class="wrap">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main" role="main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php __( 'Oops! Ova stranica nije pronađena.'); ?></h1>
-				</header><!-- .page-header -->
-				<div class="page-content">
-					<p><?php _e( 'Izgleda da ništa nije pronađeno na ovoj lokaciji. Možda probati pretragu?', 'twentyseventeen' ); ?></p>
+                <section class="error-404 not-found">
+                    <header class="page-header">
+                        <h1 class="page-title"><?php __('Oops! Ova stranica nije pronađena.'); ?></h1>
+                    </header><!-- .page-header -->
+                    <div class="page-content">
+                        <p><?php _e('Izgleda da ništa nije pronađeno na ovoj lokaciji. Nastavite sa kupovinom ili probajte pretragu', 'twentyseventeen'); ?></p>
 
-                    <a class="" href="<?php get_home_url()?>">Nastavite sa kupovinom</a>
+                        <a class="" href="/">Nastavite sa kupovinom</a>
 
-					<?php get_search_form(); ?>
 
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-		</main><!-- #main -->
-	</div><!-- #primary -->
-</div><!-- .wrap -->
+                    </div><!-- .page-content -->
+                </section><!-- .error-404 -->
+            </main><!-- #main -->
+        </div><!-- #primary -->
+    </div><!-- .wrap -->
 
 <?php
 get_footer();
