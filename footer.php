2ef86fcab2624e26c69d3e866998f57a9acf51c0
Igor Djuric <igor.djuric011@gmail.com>
2018-09-28 17:32:48 +0200

    fixed footer

--- footer.php
+++ footer.php
@@ -7,17 +7,17 @@
 <footer id="colophon" class="site-footer" role="contentinfo">
   <div class="container-fluid">
     <div class="gf-main-content-container">
-      <div class="row list-unstyled gf-footer justify-content-between">
+      <div class="row list-unstyled gf-footer justify-content-end">
         <div class="col-xs-12 col-md-3 gf-footer-section gf-footer-newsletter">
           <?php dynamic_sidebar('gf-footer-row-1-column-1'); ?>
         </div>
-        <div class="col-xs-12 col-md-3 gf-footer-section">
+        <div class="col-xs-12 col-md-3 gf-footer-section align-self-end">
           <?php dynamic_sidebar('gf-footer-row-1-column-2'); ?>
         </div>
-        <div class="col-xs-12 col-md-3 gf-footer-section">
+        <div class="col-xs-12 col-md-3 gf-footer-section align-self-end">
           <?php dynamic_sidebar('gf-footer-row-1-column-3'); ?>
         </div>
-        <div class="col-xs-12 col-md-3 gf-footer-section">
+        <div class="col-xs-12 col-md-3 gf-footer-section align-self-end">
           <?php dynamic_sidebar('gf-footer-row-1-column-4'); ?>
         </div>
       </div>
