29bea2258ac32db7867a4e6af5473295cbb56ff1
djavolak <djavolak@mail.ru>
2018-10-03 22:50:36 +0000

    compiling..

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -114,9 +114,9 @@ function gf_theme_and_plugins_frontend_scripts_and_styles()
     wp_deregister_style('searchandfilter');
     wp_dequeue_style('to-top');
     wp_deregister_style('to-top');
-    wp_dequeue_style('woocommerce-general');
-    wp_dequeue_style('woocommerce-layout');
-    wp_dequeue_style('woocommerce-smallscreen');
+//    wp_dequeue_style('woocommerce-general');
+//    wp_dequeue_style('woocommerce-layout');
+//    wp_dequeue_style('woocommerce-smallscreen');
 
 
     wp_enqueue_script('gf-front-js', get_stylesheet_directory_uri() . '/assets/js/gf-front.js', [], '', true);
