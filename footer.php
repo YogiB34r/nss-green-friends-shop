dafc3a1bbf7c0279c8ee068dde50aa40dbd6ce3e
djavolak <djavolak@mail.ru>
2018-10-07 19:48:18 +0000

    cleanup

--- footer.php
+++ footer.php
@@ -83,41 +83,16 @@
 </div>
 <!-- #page -->
 <?php wp_footer(); ?>
-<script type="text/javascript">
-    /*
-        // Add a script element as a child of the body
-        function downloadJSAtOnload() {
-            var element = document.createElement("script");
-            element.src = "deferredfunctions.js";
-            document.body.appendChild(element);
-        }
-
-        // Check for browser support of event handling capability
-        if (window.addEventListener)
-            window.addEventListener("load", downloadJSAtOnload, false);
-        else if (window.attachEvent)
-            window.attachEvent("onload", downloadJSAtOnload);
-        else window.onload = downloadJSAtOnload;
-    */
-</script>
 <!-- Facebook Pixel Code -->
 <script> !function (f, b, e, v, n, t, s) {
         if (f.fbq) return;
         n = f.fbq = function () {
             n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-        };
-        if (!f._fbq) f._fbq = n;
-        n.push = n;
-        n.loaded = !0;
-        n.version = '2.0';
-        n.queue = [];
-        t = b.createElement(e);
-        t.async = !0;
-        t.src = v;
-        s = b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t, s)
+        }; if (!f._fbq) f._fbq = n;
+        n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = []; t = b.createElement(e); t.async = !0; t.src = v;
+        s = b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t, s)
     }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '125798604755023'); // Insert your pixel ID here. fbq('track', 'PageView'); </script>
+    fbq('init', '125798604755023');</script>
 <noscript><img height="1" width="1" style="display:none"
                src="https://www.facebook.com/tr?id=125798604755023&ev=PageView&noscript=1"/>
 </noscript> <!-- DO NOT MODIFY --> <!-- End Facebook Pixel Code -->
