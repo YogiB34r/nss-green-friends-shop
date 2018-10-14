c9e071bee050cf8f0b16dad52e352bb712ca2a89
Igor Djuric <igor.djuric011@gmail.com>
2018-10-14 17:00:45 +0200

    display share buttons on single product page

--- footer.php
+++ footer.php
@@ -87,6 +87,27 @@
 <noscript><img height="1" width="1" style="display:none"
                src="https://www.facebook.com/tr?id=125798604755023&ev=PageView&noscript=1"/>
 </noscript> <!-- DO NOT MODIFY --> <!-- End Facebook Pixel Code -->
+
+<!--facebook share-->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+<!--Pinterest share-->
+<script type="text/javascript" async defer src="//assets.pinterest.com/js/pinit.js"></script>
+
+<!--Google+ share-->
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+
+<!--Twitter share-->
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+
 </body>
 </html>
 
--- inc/gf-cart-page-functions.php
+++ inc/gf-cart-page-functions.php
@@ -1,50 +1,4 @@
 <?php
-/**
- * Display shipping category and price
- */
-add_filter('woocommerce_package_rates', 'gf_custom_shipping_rates', 10, 2);
-function gf_custom_shipping_rates($rates, $package) {
-    if (WC()->cart->cart_contents_weight <= 0.5) {
-        if (isset($rates['flat_rate:3']))
-            unset($rates['flat_rate:4'], $rates['flat_rate:5'], $rates['flat_rate:6'], $rates['flat_rate:7'],
-                $rates['flat_rate:8'], $rates['flat_rate:9'], $rates['flat_rate:10']);
-    } elseif (WC()->cart->cart_contents_weight > 0.5 and WC()->cart->cart_contents_weight <= 2) {
-        if (isset($rates['flat_rate:4']))
-            unset($rates['flat_rate:3'], $rates['flat_rate:5'], $rates['flat_rate:6'], $rates['flat_rate:7'],
-                $rates['flat_rate:8'], $rates['flat_rate:9'], $rates['flat_rate:10']);
-    } elseif (WC()->cart->cart_contents_weight > 2 and WC()->cart->cart_contents_weight <= 5) {
-        if (isset($rates['flat_rate:5']))
-            unset($rates['flat_rate:3'], $rates['flat_rate:4'], $rates['flat_rate:6'], $rates['flat_rate:7'],
-                $rates['flat_rate:8'], $rates['flat_rate:9'], $rates['flat_rate:10']);
-    } elseif (WC()->cart->cart_contents_weight > 5 and WC()->cart->cart_contents_weight <= 10) {
-        if (isset($rates['flat_rate:6']))
-            unset($rates['flat_rate:3'], $rates['flat_rate:4'], $rates['flat_rate:5'], $rates['flat_rate:7'],
-                $rates['flat_rate:8'], $rates['flat_rate:9'], $rates['flat_rate:10']);
-    } elseif (WC()->cart->cart_contents_weight > 10 and WC()->cart->cart_contents_weight <= 20) {
-        if (isset($rates['flat_rate:7']))
-            unset($rates['flat_rate:3'], $rates['flat_rate:4'], $rates['flat_rate:5'], $rates['flat_rate:6'],
-                $rates['flat_rate:8'], $rates['flat_rate:9'], $rates['flat_rate:10']);
-    } elseif (WC()->cart->cart_contents_weight > 20 and WC()->cart->cart_contents_weight <= 30) {
-        if (isset($rates['flat_rate:8']))
-            unset($rates['flat_rate:3'], $rates['flat_rate:4'], $rates['flat_rate:5'], $rates['flat_rate:6'],
-                $rates['flat_rate:7'], $rates['flat_rate:9'], $rates['flat_rate:10']);
-    } elseif (WC()->cart->cart_contents_weight > 30 and WC()->cart->cart_contents_weight <= 50) {
-        if (isset($rates['flat_rate:9']))
-            unset($rates['flat_rate:3'], $rates['flat_rate:4'], $rates['flat_rate:5'], $rates['flat_rate:6'],
-                $rates['flat_rate:7'],$rates['flat_rate:8'],$rates['flat_rate:10']);
-    } elseif (WC()->cart->cart_contents_weight > 50) {
-        if (isset($rates['flat_rate:10'])) {
-            $cartWeight = WC()->cart->cart_contents_weight;
-            $myExtraWeight = $cartWeight - 50;
-            $myNewPrice = 500 + (10 * $myExtraWeight);
-            $rates['flat_rate:10']->set_cost($myNewPrice);
-
-            unset($rates['flat_rate:3'],$rates['flat_rate:4'],$rates['flat_rate:5'],$rates['flat_rate:6'],$rates['flat_rate:7'],
-                $rates['flat_rate:8'],$rates['flat_rate:9']);
-        }
-    }
-    return $rates;
-}
 
 add_action('woocommerce_before_cart_table', 'gf_cart_display_tax_notice', 10);
 function gf_cart_display_tax_notice()
@@ -67,7 +21,7 @@ function gf_cart_display_seller_info()
 
 
 /*
- * Display total cart weight on cart & checkout page
+ * Display total cart weight on cart & order page
  */
 add_action('woocommerce_review_order_before_shipping', 'bbloomer_print_cart_weight');
 add_action('woocommerce_cart_totals_before_shipping', 'bbloomer_print_cart_weight');
