24a0cdc4d2de2b417467a3b30492be1acdae9e3f
Igor Djuric <igor.djuric011@gmail.com>
2018-10-16 17:51:42 +0200

    new footer fixed

--- footer.php
+++ footer.php
@@ -11,64 +11,47 @@
                 <div class="col-xs-12 col-md-3 gf-footer-section gf-footer-newsletter">
                     <?php dynamic_sidebar('gf-footer-row-1-column-1'); ?>
                 </div>
-                <div class="col-xs-12 col-md-3 gf-footer-section align-self-end">
-                    <?php dynamic_sidebar('gf-footer-row-1-column-2'); ?>
-                </div>
-                <div class="col-xs-12 col-md-3 gf-footer-section align-self-end">
-                    <?php dynamic_sidebar('gf-footer-row-1-column-3'); ?>
-                </div>
-                <div class="col-xs-12 col-md-3 gf-footer-section align-self-end">
-                    <?php dynamic_sidebar('gf-footer-row-1-column-4'); ?>
-                </div>
-            </div>
-            <div class="row list-unstyled">
-                <div class="col-9">
-                    <?php dynamic_sidebar('gf-footer-row-2-column-1'); ?>
-                </div>
-                <div class="col-3">
-                    <?php dynamic_sidebar('gf-footer-row-2-column-2'); ?>
-                </div>
-            </div>
-            <div class="row list-unstyled gf-footer gf-footer-images-wrapper">
-                <div class="my-footer-images">
-                    <?php $theme_dir = get_template_directory_uri(); ?>
-                    <div class="row">
-                        <ul class="col-md d-flex gf-justify justify-content-center mt-3 px-4">
-                            <li>
+                <div class="col-xs-12 col-md-9 gf-footer-section">
+                    <div class="row gf-footer-links-wrapper">
+                        <a class="mr-3 mt-2" href="/o-kompaniji/">O kompaniji</a>
+                        <a class="mr-3 mt-2" href="/kontaktirajte-nas/">Kontaktirajte nas</a>
+
+                            <?php if (is_user_logged_in()) {
+                                echo '<a class="mr-3 mt-2" href="/moj-nalog/narudzbine/">Praćenje narudžbenice</a>';
+                            } else {
+                                '<a class="mr-3 mt-2" href="/pracenje-narudzbenice/">Praćenje narudžbenice</a>';
+                            }
+                            ?>
+
+
+                        <a class="mr-3 mt-2" href="/podrska/">Podrška</a>
+                        <a class="mr-3 mt-2" href="/uslovi-kupovine/">Uslovi kupovine</a>
+                        <a class="mr-3 mt-2" href="/politika-privatnosti-2/">Politika privatnosti</a>
+
+                        </div>
+                        <div class="row mt-4 gf-footer-card-images-wrapper">
+                            <div class="">
                                 <div class="gffooter-card-3"></div>
-                            </li>
-                            <li>
                                 <div class="gffooter-card-4"></div>
-                            </li>
-                            <li>
                                 <div class="gffooter-card-5"></div>
-                            </li>
-                            <li>
                                 <div class="gffooter-card-6"></div>
-                            </li>
-                        </ul>
-                        <div class="col d-flex justify-content-center mt-3 px-4">
-                            <div class="row">
-                                <div class="col-md col-sm col-xs text-center">
-                                    <a href="https://www.visa.ca/en_CA/run-your-business/merchant-resources/verified-by-visa.html">
-                                        <div class="gffooter-card-1"></div>
-                                    </a>
-                                    <a href="https://www.mastercard.us/en-us/merchants/safety-security/securecode.html">
-                                        <div class="gffooter-card-2"></div>
-                                    </a>
-                                </div>
-                                <div class="col-md col-sm col-xs  text-center">
-                                    <a href="http://www.bancaintesa.rs/pocetna.1.html">
-                                        <div class="gffooter-card-7"></div>
-                                    </a>
-                                </div>
+                            </div>
+                            <div class="">
+                                <a href="https://www.visa.ca/en_CA/run-your-business/merchant-resources/verified-by-visa.html">
+                                    <div class="gffooter-card-1"></div>
+                                </a>
+                                <a href="https://www.mastercard.us/en-us/merchants/safety-security/securecode.html">
+                                    <div class="gffooter-card-2"></div>
+                                </a>
+                                <a href="http://www.bancaintesa.rs/pocetna.1.html">
+                                    <div class="gffooter-card-7"></div>
+                                </a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 </footer>
 <!-- #colophon -->
 </div>
@@ -79,9 +62,17 @@
         if (f.fbq) return;
         n = f.fbq = function () {
             n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-        }; if (!f._fbq) f._fbq = n;
-        n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = []; t = b.createElement(e); t.async = !0; t.src = v;
-        s = b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t, s)
+        };
+        if (!f._fbq) f._fbq = n;
+        n.push = n;
+        n.loaded = !0;
+        n.version = '2.0';
+        n.queue = [];
+        t = b.createElement(e);
+        t.async = !0;
+        t.src = v;
+        s = b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t, s)
     }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
     fbq('init', '125798604755023');</script>
 <noscript><img height="1" width="1" style="display:none"
@@ -90,10 +81,11 @@
 
 <!--facebook share-->
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
+<script>(function (d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
+        js = d.createElement(s);
+        js.id = id;
         js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1';
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
--- inc/gf-registred-sidebars.php
+++ inc/gf-registred-sidebars.php
@@ -1,113 +1,84 @@
 <?php
-add_action( 'widgets_init', 'gf_register_sidebars' );
-function gf_register_sidebars() {
-    $theme= wp_get_theme();
+add_action('widgets_init', 'gf_register_sidebars');
+function gf_register_sidebars()
+{
+    $theme = wp_get_theme();
     $my_sidebars = array(
         array(
-            'name'          => __('Header Text/Registracija/Prijava', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-header-row-1',
-            'description'   => '',
+            'name' => __('Header Text/Registracija/Prijava', '' . $theme->get('TextDomain') . ''),
+            'id' => 'gf-header-row-1',
+            'description' => '',
         ),
         array(
-            'name'          => __('Header Logo', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-header-row-2-col-1',
-            'description'   => '',
+            'name' => __('Header Logo', '' . $theme->get('TextDomain') . ''),
+            'id' => 'gf-header-row-2-col-1',
+            'description' => '',
         ),
         array(
-            'name'          => __('Header Search form (desktop)', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-header-row-2-col-2',
-            'description'   => '',
+            'name' => __('Header Search form (desktop)', '' . $theme->get('TextDomain') . ''),
+            'id' => 'gf-header-row-2-col-2',
+            'description' => '',
         ),
         array(
-            'name'          => __('Search form mobile', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-search-form-mobile',
-            'description'   => '',
+            'name' => __('Search form mobile', '' . $theme->get('TextDomain') . ''),
+            'id' => 'gf-search-form-mobile',
+            'description' => '',
         ),
         array(
-            'name'          => __('Header cart/account/mobile navigation', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-header-row-2-col-3',
-            'description'   => '',
+            'name' => __('Header cart/account/mobile navigation', '' . $theme->get('TextDomain') . ''),
+            'id' => 'gf-header-row-2-col-3',
+            'description' => '',
         ),
         array(
-            'name'          => __('Home images slider/banners(desktop)', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-homepage-row-1',
-            'description'   => '',
+            'name' => __('Home images slider/banners(desktop)', '' . $theme->get('TextDomain') . ''),
+            'id' => 'gf-homepage-row-1',
+            'description' => '',
         ),
         array(
-            'name'          => __('Home images slider/banners(mobile)', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-homepage-row-1-mobile',
-            'description'   => '',
+            'name' => __('Home images slider/banners(mobile)', '' . $theme->get('TextDomain') . ''),
+            'id' => 'gf-homepage-row-1-mobile',
+            'description' => '',
         ),
         array(
-            'name'          => __('Homepage product sliders', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-homepage-row-2',
-            'description'   => '',
+            'name' => __('Homepage product sliders', '' . $theme->get('TextDomain') . ''),
+            'id' => 'gf-homepage-row-2',
+            'description' => '',
         ),
         array(
-            'name'          => __('Homepage product sliders (mobile)', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-homepage-row-3',
-            'description'   => '',
+            'name' => __('Homepage product sliders (mobile)', '' . $theme->get('TextDomain') . ''),
+            'id' => 'gf-homepage-row-3',
+            'description' => '',
         ),
         array(
-            'name'          => __('Footer Newsletter', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-footer-row-1-column-1',
-            'description'   => 'First footer row column 1',
+            'name' => __('Footer Newsletter', '' . $theme->get('TextDomain') . ''),
+            'id' => 'gf-footer-row-1-column-1',
+            'description' => 'First footer row column 1',
         ),
         array(
-            'name'          => __('Footer O kompaniji', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-footer-row-1-column-2',
-            'description'   => 'First footer row column 2',
-        ),
-        array(
-            'name'          => __('Footer Podrška', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-footer-row-1-column-3',
-            'description'   => 'First footer row column 3',
-        ),
-        array(
-            'name'          => __('Footer Uslovi korišćenja', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-footer-row-1-column-4',
-            'description'   => 'First footer row column 4',
-        ),
-        array(
-            'name'          => __('Footer row 2 column 1', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-footer-row-2-column-1',
-            'description'   => 'Second footer row column 1',
-        ),
-        array(
-            'name'          => __('Footer row 2 column 2', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-footer-row-2-column-2',
-            'description'   => 'Second footer row column 2',
-        ),
-        array(
-            'name'          => __('Footer Cards Images', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-footer-row-3-column-1',
-            'description'   => 'Third footer row column 1',
-        ),
-        array(
-            'name'          => __('Left sidebar', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-left-sidebar',
-            'description'   => 'Left sidebar',
+            'name' => __('Left sidebar', '' . $theme->get('TextDomain') . ''),
+            'id' => 'gf-left-sidebar',
+            'description' => 'Left sidebar',
 
         ),
         array(
-            'name'          => __('Category Sidebar', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-category-sidebar',
-            'description'   => 'Category page sidebar',
+            'name' => __('Category Sidebar', '' . $theme->get('TextDomain') . ''),
+            'id' => 'gf-category-sidebar',
+            'description' => 'Category page sidebar',
         ),
         array(
-            'name'          => __('Category Sidebar (product filters for mobile)', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-category-sidebar-product-filters',
-            'description'   => 'Category page sidebar (product filters for mobile)',
+            'name' => __('Category Sidebar (product filters for mobile)', '' . $theme->get('TextDomain') . ''),
+            'id' => 'gf-category-sidebar-product-filters',
+            'description' => 'Category page sidebar (product filters for mobile)',
         ),
         array(
-            'name'          => __('Sidebar (single product page)', ''.$theme->get('TextDomain').''),
-            'id'            => 'gf-sidebar-single-productpage',
-            'description'   => 'Sidebar (single product page)',
+            'name' => __('Sidebar (single product page)', '' . $theme->get('TextDomain') . ''),
+            'id' => 'gf-sidebar-single-productpage',
+            'description' => 'Sidebar (single product page)',
         ),
     );
 
 
-    foreach( $my_sidebars as $args ) {
+    foreach ($my_sidebars as $args) {
         register_sidebar($args);
     }
 }
