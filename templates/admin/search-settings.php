ed1d17725484b41aae452ffef394f6e8ff09b891
Igor Djuric <igor.djuric011@gmail.com>
2018-10-17 19:40:45 +0200

    search manager custom wp table

--- templates/admin/search-settings.php
+++ templates/admin/search-settings.php
@@ -3,10 +3,10 @@
  * @var Elastica\Result $result
  */
 
-if(is_admin())
-{
+if (is_admin()) {
     new Gf_Search_Wp_List_Table();
 }
+
 /**
  * Paulund_Wp_List_Table class will create the page to load the table
  */
@@ -27,21 +27,23 @@ class Gf_Search_Wp_List_Table
      */
     public function list_table_page()
     {
-        $exampleListTable = new Search_List_Table();
-        $exampleListTable->prepare_items();
+        $searchListTable = new Search_List_Table();
+        $searchListTable->prepare_items();
         ?>
         <div class="wrap">
             <div id="icon-users" class="icon32"></div>
-            <h2>Example List Table Page</h2>
-            <?php $exampleListTable->display(); ?>
+            <h2>Podešavanje pretrage</h2>
+            <?php $searchListTable->display(); ?>
         </div>
         <?php
     }
 }
+
 // WP_List_Table is not loaded automatically so we need to load it in our application
-if( ! class_exists( 'WP_List_Table' ) ) {
-    require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+if (!class_exists('WP_List_Table')) {
+    require_once(ABSPATH . 'wp-admin/includes/class-wp-list-table.php');
 }
+
 /**
  * Create a new table class that will extend the WP_List_Table
  */
@@ -57,19 +59,24 @@ class Search_List_Table extends WP_List_Table
         $columns = $this->get_columns();
         $hidden = $this->get_hidden_columns();
         $sortable = $this->get_sortable_columns();
+
+        /** Process bulk action */
+        $this->process_bulk_action();
+
         $data = $this->table_data();
-        usort( $data, array( &$this, 'sort_data' ) );
+        usort($data, array(&$this, 'sort_data'));
         $perPage = 10;
         $currentPage = $this->get_pagenum();
         $totalItems = count($data);
-        $this->set_pagination_args( array(
+        $this->set_pagination_args(array(
             'total_items' => $totalItems,
-            'per_page'    => $perPage
-        ) );
-        $data = array_slice($data,(($currentPage-1)*$perPage),$perPage);
+            'per_page' => $perPage
+        ));
+        $data = array_slice($data, (($currentPage - 1) * $perPage), $perPage);
         $this->_column_headers = array($columns, $hidden, $sortable);
         $this->items = $data;
     }
+
     /**
      * Override the parent columns method. Defines the columns to use in your listing table
      *
@@ -78,13 +85,14 @@ class Search_List_Table extends WP_List_Table
     public function get_columns()
     {
         $columns = array(
-            'upit'              => 'UPIT',
-            'broj_ponavljanja'  => 'Broj Ponavljanja',
-            'preusmeran_na'     => 'Preusmeren Na',
-            'akcija'            => 'Akcija',
+            'cb' => '<input type="checkbox" />',
+            'searchQuery' => 'Upit',
+            'count' => 'Broj Ponavljanja',
+            'url' => 'Preusmeren Na',
         );
         return $columns;
     }
+
     /**
      * Define which columns are hidden
      *
@@ -94,6 +102,7 @@ class Search_List_Table extends WP_List_Table
     {
         return array();
     }
+
     /**
      * Define the sortable columns
      *
@@ -101,8 +110,12 @@ class Search_List_Table extends WP_List_Table
      */
     public function get_sortable_columns()
     {
-        return array('title' => array('title', false));
+        return array(
+            'searchQuery' => array('searchQuery', false),
+            'count' => array('count', false)
+        );
     }
+
     /**
      * Get the table data
      *
@@ -110,68 +123,123 @@ class Search_List_Table extends WP_List_Table
      */
     private function table_data()
     {
-        $data = array();
-        $data[] = array(
-            'id'          => 1,
-            'title'       => 'The Shawshank Redemption',
-            'description' => 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-            'year'        => '1994',
-            'director'    => 'Frank Darabont',
-            'rating'      => '9.3'
+        $config = array(
+            'host' => ES_HOST,
+            'port' => 9200
         );
-        
+        $client = new \Elastica\Client($config);
+        $term = new \GF\Search\Elastica\TermSearch($client);
+        $data = [];
+        foreach ($term->getTerms() as $term) {
+            $data[] = array(
+                'id' => $term->id,
+                'searchQuery' => $term->searchQuery,
+                'count' => $term->count,
+                'url' => $term->url
+            );
+        }
+
+
         return $data;
     }
+
     /**
      * Define what data to show on each column of the table
      *
-     * @param  Array $item        Data
+     * @param  Array $item Data
      * @param  String $column_name - Current column name
      *
      * @return Mixed
      */
-    public function column_default( $item, $column_name )
+    public function column_default($item, $column_name)
     {
-        switch( $column_name ) {
+        switch ($column_name) {
             case 'id':
-            case 'title':
-            case 'description':
-            case 'year':
-            case 'director':
-            case 'rating':
-                return $item[ $column_name ];
+            case 'searchQuery':
+            case 'count':
+            case 'url':
+                return $item[$column_name];
             default:
-                return print_r( $item, true ) ;
+                return print_r($item, true);
         }
     }
+
     /**
      * Allows you to sort the data by the variables set in the $_GET
      *
      * @return Mixed
      */
-    private function sort_data( $a, $b )
+    private function sort_data($a, $b)
     {
         // Set defaults
-        $orderby = 'title';
+        $orderby = 'searchQuery';
         $order = 'asc';
         // If orderby is set, use this as the sort column
-        if(!empty($_GET['orderby']))
-        {
+        if (!empty($_GET['orderby'])) {
             $orderby = $_GET['orderby'];
         }
         // If order is set use this as the order
-        if(!empty($_GET['order']))
-        {
+        if (!empty($_GET['order'])) {
             $order = $_GET['order'];
         }
-        $result = strcmp( $a[$orderby], $b[$orderby] );
-        if($order === 'asc')
-        {
+        $result = strcmp($a[$orderby], $b[$orderby]);
+        if ($order === 'asc') {
             return $result;
         }
         return -$result;
     }
+
+    function column_url($item)
+    {
+        $actions = array(
+            'edit' => sprintf('<a href="?page=%s&action=%s&url=%s">Edit</a>', $_REQUEST['page'], 'edit', $item['id']),
+        );
+
+        return sprintf('%1$s %2$s', $item['url'], $this->row_actions($actions));
+    }
+
+    function get_bulk_actions()
+    {
+        $actions = array(
+            'delete' => 'Delete'
+        );
+        return $actions;
+    }
+
+    function column_cb($item)
+    {
+        return sprintf(
+            '<input type="checkbox" name="searchQuery[]" value="%s" />', $item['id']
+        );
+    }
+
+    /**
+     * Delete a query record.
+     *
+     * @param int $id query ID
+     */
+    public static function delete_searchQuery( $id ) {
+        global $wpdb;
+
+        $wpdb->delete(
+            "{$wpdb->prefix}",
+            [ 'id' => $id ],
+            [ '%d' ]
+        );
+        //@TODO finish delete action
+    }
+
+    public function process_bulk_action()
+    {
+        //@TODO process bulk delete action
+    }
+
+
+
 }
+
+
+
 ?>
 
 <h3>Pregled snimljenih upita</h3>
@@ -184,32 +252,38 @@ class Search_List_Table extends WP_List_Table
         <th>Preusmeren na</th>
         <th>Akcija</th>
     </tr>
-    <?php var_dump($term->getTerms());?>
-<?php foreach ($term->getTerms() as $result): ?>
-<?php
-    $url = '<input type="text" style="display:none;" />';
-    if ($result->getData()['url'] !== '') {
-        $url = '<input readonly class="redirected" type="text" value="'. $result->getData()['url'] .'" />';
-    }
-?>
-    <tr>
-        <td><?=$result->getData()['searchQuery']?></td>
-        <td><?=$result->getData()['count']?></td>
-        <td><?=$url?> <button style="display: none" data-query="<?=$result->getData()['searchQuery']?>" class="redirect">Snimi</button></td>
-        <td><a href="#" class="showRedirect">Preusmeri</a></td>
-    </tr>
-<?php endforeach;?>
+    <?php
+    var_dump($_GET);
+    ?>
+    <?php foreach ($term->getTerms() as $result): ?>
+        <?php
+        $url = '<input type="text" style="display:none;" />';
+        if ($result->getData()['url'] !== '') {
+            $url = '<input readonly class="redirected" type="text" value="' . $result->getData()['url'] . '" />';
+        }
+        ?>
+        <tr>
+            <td><?= $result->getData()['searchQuery'] ?></td>
+            <td><?= $result->getData()['count'] ?></td>
+            <td><?= $url ?>
+                <button style="display: none" data-query="<?= $result->getData()['searchQuery'] ?>" class="redirect">
+                    Snimi
+                </button>
+            </td>
+            <td><a href="#" class="showRedirect">Preusmeri</a></td>
+        </tr>
+    <?php endforeach; ?>
 </table>
 
 <script>
-    jQuery(document).ready(function() {
-        jQuery('.redirect').click(function() {
+    jQuery(document).ready(function () {
+        jQuery('.redirect').click(function () {
             var parent = jQuery(this).parent();
             var data = {
                 term: jQuery(this).data('query'),
                 url: parent.find('input').val()
             };
-            jQuery.post('/gf-ajax/?saveSearchRedirect=1', data, function(response) {
+            jQuery.post('/gf-ajax/?saveSearchRedirect=1', data, function (response) {
                 if (response == 1) {
                     parent.find('input').prop('readonly', true);
                     parent.find('button').hide();
@@ -218,7 +292,7 @@ class Search_List_Table extends WP_List_Table
             });
         });
 
-        jQuery('.showRedirect').click(function() {
+        jQuery('.showRedirect').click(function () {
             var inputElement = jQuery(this).parent().prev().find('input');
             if (inputElement.hasClass('redirected')) {
                 inputElement.prop('readonly', false);
