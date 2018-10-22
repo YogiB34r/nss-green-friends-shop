c7b1cef4ae71e5026ef4ab73beebc1c138f4b37d
Igor Djuric <igor.djuric011@gmail.com>
2018-10-22 17:30:55 +0200

    Trying to crate users

--- front-page.php
+++ front-page.php
@@ -1,27 +1,189 @@
-<?php get_header();?>
+<?php get_header();
+
+$file = file_get_contents(__DIR__ . '/users1.csv');
+$lines = explode(PHP_EOL, $file);
+unset($lines[0]);
+global $wpdb;
+foreach ($lines as $line){
+    $data = explode(',', $line);
+
+    $username = '';
+    $email = '';
+    $password = '';
+    $first_name = '';
+    $billing_first_name = '';
+    $billing_company = '';
+    $billing_pib = '';
+    $billing_mb = '';
+    $billing_address_1 = '';
+    $billing_city = '';
+    $billing_postcode = '';
+    $billing_phone = '';
+
+    $shipping_first_name = '';
+    $shipping_company = '';
+    $shipping_address_1 = '';
+    $shipping_city = '';
+    $shipping_postcode = '';
+    $shipping_phone = '';
+
+    $billing_country = 'RS';
+    $shipping_country = 'RS';
+
+
+    $age_range = '';
+    $meta_sex = '';
+    $migrated = '';
+
+    if (!empty($data[0])) {
+        $username = $data[0];
+    }
+    if (!empty($data[1])) {
+        $email = $data[1];
+    }
+    if (!empty($data[2])) {
+        $password = $data[2];
+    }
+    if (!empty($data[3])) {
+        $first_name = $data[3];
+    }
+    if (!empty($data[4])) {
+        $billing_first_name = $data[4];
+    }
+    if (!empty($data[5])) {
+        $billing_company = $data[5];
+    }
+    if (!empty($data[6])) {
+        $billing_pib = $data[6];
+    }
+    if (!empty($data[7])) {
+        $billing_mb = $data[7];
+    }
+    if (!empty($data[8])) {
+        $billing_address_1 = $data[8];
+    }
+    if (!empty($data[8])) {
+        $billing_address_1 = $data[8];
+    }
+    if (!empty($data[9])) {
+        $billing_city = $data[9];
+    }
+    if (!empty($data[10])) {
+        $billing_postcode = $data[10];
+    }
+    if (!empty($data[11])) {
+        $billing_phone = $data[11];
+    }
+
+    if (!empty($data[12])) {
+        $shipping_first_name = $data[12];
+    }
+    if (!empty($data[13])) {
+        $shipping_company = $data[13];
+    }
+    if (!empty($data[14])) {
+        $shipping_address_1 = $data[14];
+    }
+    if (!empty($data[15])) {
+        $shipping_city = $data[15];
+    }
+    if (!empty($data[16])) {
+        $shipping_postcode = $data[16];
+    }
+    if (!empty($data[17])) {
+        $shipping_phone = $data[17];
+    }
+
+    $billing_country = 'RS';
+    $shipping_country = 'RS';
+
+    if (!empty($data[20])) {
+        $age_range = $data[20];
+    }
+    if (!empty($data[21])) {
+        $meta_sex = $data[21];
+    }
+    if (!empty($data[22])) {
+        $migrated = $data[22];
+    }
+//    $user_id = wc_create_new_customer($email, $username, $password);
+
+    $user_data = array(
+        'user_login' => $email,
+        'user_pass' => $password,
+        'user_email' => $email,
+        'role' => 'customer'
+    );
+
+    $user_id = wp_insert_user($user_data);
+
+    $salt = 'd@uy/o%b^';
+    $passwordHash = $salt . md5($salt, $password);
+    $sql_update_pass = "UPDATE wp_users SET user_pass = '{$passwordHash}' WHERE ID = '{$user_id}'";
+    $update = $wpdb->query($sql_update_pass);
+
+    $sql_get_pass = "SELECT user_pass FROM wp_users WHERE ID = '{$user_id}'";
+    $password_in_db = $wpdb->get_results($sql_get_pass)[0]->user_pass;
+
+    var_dump($passwordHash);
+    var_dump($password_in_db);
+
+
+    update_user_meta($user_id, "first_name", $first_name);
+
+    //billing
+    update_user_meta($user_id, "billing_first_name", $billing_first_name);
+    update_user_meta($user_id, "billing_company", $billing_company);
+    update_user_meta($user_id, "billing_pib", $billing_pib);
+    update_user_meta($user_id, "billing_mb", $billing_mb);
+    update_user_meta($user_id, "billing_address_1", $billing_address_1);
+    update_user_meta($user_id, "billing_city", $billing_city);
+    update_user_meta($user_id, "billing_postcode", $billing_postcode);
+    update_user_meta($user_id, "billing_phone", $billing_phone);
+    update_user_meta($user_id, "billing_country", $billing_country);
+
+    //shipping
+    update_user_meta($user_id, "shipping_first_name", $shipping_first_name);
+    update_user_meta($user_id, "shipping_company", $shipping_company);
+    update_user_meta($user_id, "shipping_address_1", $shipping_address_1);
+    update_user_meta($user_id, "shipping_city", $shipping_city);
+    update_user_meta($user_id, "shipping_postcode", $shipping_postcode);
+    update_user_meta($user_id, "shipping_phone", $shipping_phone);
+    update_user_meta($user_id, "shipping_country", $shipping_country);
+
+    update_user_meta($user_id, "age_range", $age_range);
+    update_user_meta($user_id, "meta_sex", $meta_sex);
+    update_user_meta($user_id, "migrated", $migrated);
+
+
+
+
+}
+
+?>
 <div class="row">
-  <div class="col-3 gf-sidebar gf-left-sidebar">
-    <div class="gf-left-sidebar-wrapper">
-      <div class="gf-wrapper-before">
-        <div class="gf-category-sidebar-toggle">Kategorije</div>
-        <span class="fas fa-angle-up"></span>
-      </div>
-      <?php dynamic_sidebar('gf-left-sidebar'); ?>
+    <div class="col-3 gf-sidebar gf-left-sidebar">
+        <div class="gf-left-sidebar-wrapper">
+            <div class="gf-wrapper-before">
+                <div class="gf-category-sidebar-toggle">Kategorije</div>
+                <span class="fas fa-angle-up"></span>
+            </div>
+            <?php dynamic_sidebar('gf-left-sidebar'); ?>
+        </div>
     </div>
-  </div>
-  <div class="gf-content-wrapper col-md-9 col-sm-12">
-    <div class="gf-row row list-unstyled gf-image-slider-banners-desktop">
-      <?php dynamic_sidebar('gf-homepage-row-1'); ?>
+    <div class="gf-content-wrapper col-md-9 col-sm-12">
+        <div class="gf-row row list-unstyled gf-image-slider-banners-desktop">
+            <?php dynamic_sidebar('gf-homepage-row-1'); ?>
+        </div>
+        <div class="gf-row row list-unstyled gf-image-slider-banners-mobile">
+            <?php dynamic_sidebar('gf-homepage-row-1-mobile'); ?>
+        </div>
+        <div class="gf-row row list-unstyled gf-product-sliders-desktop-version">
+            <?php dynamic_sidebar('gf-homepage-row-2'); ?>
+        </div>
+        <div class="gf-row row list-unstyled gf-product-sliders-mobile-version">
+            <?php dynamic_sidebar('gf-homepage-row-3'); ?>
+        </div>
     </div>
-      <div class="gf-row row list-unstyled gf-image-slider-banners-mobile">
-          <?php dynamic_sidebar('gf-homepage-row-1-mobile'); ?>
-      </div>
-    <div class="gf-row row list-unstyled gf-product-sliders-desktop-version">
-      <?php dynamic_sidebar('gf-homepage-row-2'); ?>
-    </div>
-    <div class="gf-row row list-unstyled gf-product-sliders-mobile-version">
-      <?php dynamic_sidebar('gf-homepage-row-3'); ?>
-    </div>
-  </div>
 </div>
 <?php get_footer(); ?>
--- user.functions.php
+++ user.functions.php
@@ -1,13 +1,15 @@
 <?php
 
-function gf_add_custom_meta_to_users() {
+function gf_add_custom_meta_to_users()
+{
     $users = get_users(array('fields' => array('ID')));
     foreach ($users as $user) {
         update_user_meta($user->ID, 'migrated', '0');
     }
 }
 
-function gf_check_if_user_is_migrated($user, $password) {
+function gf_check_if_user_is_migrated($user, $password)
+{
     if (!empty($user)) {
         if (get_user_meta($user->ID, 'migrated', true) != 0) {
 
@@ -21,15 +23,10 @@ function gf_check_if_user_is_migrated($user, $password) {
             $sql = "SELECT user_pass FROM wp_users WHERE ID = {$user->ID}";
             $password_in_db = $wpdb->get_results($sql)[0]->user_pass;
 
-//            var_dump($passwordHash);
-//            var_dump($password_in_db);
-//            var_dump($user);
-//            die();
 
             if ($passwordHash === $password_in_db) {
                 return $user;
             } else {
-                var_dump('caoo');
                 return new WP_Error('incorrect_password',
                     sprintf(
                     /* translators: %s: user name */
@@ -47,69 +44,54 @@ function gf_check_if_user_is_migrated($user, $password) {
     return false;
 }
 
-remove_filter( 'authenticate', 'wp_authenticate_username_password' );
-add_filter( 'authenticate', 'gf_authenticate_username_password', 20, 3 );
+remove_filter('authenticate', 'wp_authenticate_username_password');
+add_filter('authenticate', 'gf_authenticate_username_password', 20, 3);
 /**
  * Remove Wordpress filer and write our own with changed error text.
  */
-function gf_authenticate_username_password( $user, $username, $password ) {
-    if ( is_a($user, 'WP_User') )
+function gf_authenticate_username_password($user, $username, $password)
+{
+    if (is_a($user, 'WP_User'))
         return $user;
 
-    if ( empty( $username ) || empty( $password ) ) {
-        if ( is_wp_error( $user ) )
+    if (empty($username) || empty($password)) {
+        if (is_wp_error($user))
             return $user;
 
         $error = new WP_Error();
 
-        if ( empty( $username ) )
-            return new WP_Error( 'invalid_username', sprintf( __( '<strong>GREŠKA</strong>: Polje korisničko ime ne može biti prazno.' ), wp_lostpassword_url() ) );
+        if (empty($username))
+            return new WP_Error('invalid_username', sprintf(__('<strong>GREŠKA</strong>: Polje korisničko ime ne može biti prazno.'), wp_lostpassword_url()));
 
-        if ( empty( $password ) )
-            return new WP_Error( 'invalid_username', sprintf( __( '<strong>GREŠKA</strong>: Polje lozinka ne može biti prazno.' ), wp_lostpassword_url() ) );
+        if (empty($password))
+            return new WP_Error('invalid_username', sprintf(__('<strong>GREŠKA</strong>: Polje lozinka ne može biti prazno.'), wp_lostpassword_url()));
 
         return $error;
     }
 
-    if ( !$user )
-        return new WP_Error( 'invalid_username', sprintf( __( '<strong>GREŠKA</strong>: Nepostojeće korisničko ime ili email. <a href="%s" title="Lozinka izgubljena">Izgubili ste lozinku</a>?' ), wp_lostpassword_url() ) );
-
-    $user = apply_filters( 'wp_authenticate_user', $user, $password );
-    if ( is_wp_error( $user ) )
+    if (!$user)
+        return new WP_Error('invalid_username', sprintf(__('<strong>GREŠKA</strong>: Nepostojeće korisničko ime ili email. <a href="%s" title="Lozinka izgubljena">Izgubili ste lozinku</a>?'), wp_lostpassword_url()));
+//
+    $user = apply_filters('wp_authenticate_user', $user, $password);
+    if (is_wp_error($user))
         return $user;
 
-    $user = get_user_by( 'login', $username );
-
-    if(get_user_meta($user->ID, 'migrated', true) != 0){
-        global $wpdb;
-        $salt = 'd@uy/o%b^';
-        $passwordHash = $salt . md5($salt, $password);
-        var_dump('ovde sam');
-        var_dump($passwordHash);
-        $sql = "SELECT user_pass FROM wp_users WHERE ID = {$user->ID}";
-        $password_in_db = $wpdb->get_results($sql)[0]->user_pass;
-        if($passwordHash === $password_in_db){
-            return $user;
-        }
-    }else{
-        if ( ! wp_check_password( $password, $user->user_pass, $user->ID ) )
-            return new WP_Error( 'incorrect_password', sprintf( __( '<strong>GREŠKA</strong>: Lozinka koju ste uneli za korisničko ime <strong>%1$s</strong> nije ispravna. <a href="%2$s" title="Lozinka izgubljena">Izgubili ste lozinku</a>?' ),
-                $username, wp_lostpassword_url() ) );
-    }
-
+    if (!wp_check_password($password, $user->user_pass, $user->ID))
+        return new WP_Error('incorrect_password', sprintf(__('<strong>GREŠKA</strong>: Lozinka koju ste uneli za korisničko ime <strong>%1$s</strong> nije ispravna. <a href="%2$s" title="Lozinka izgubljena">Izgubili ste lozinku</a>?'),
+            $username, wp_lostpassword_url()));
 
 
     return $user;
 }
 
-add_action('validate_password_reset', 'gf_validate_password_reset', 10, 2 );
-function gf_validate_password_reset( $errors, $user ) {
-    if(strlen($_POST['password_1']) < 6  ) {
-        $errors->add( 'woocommerce_password_error', __( 'Lozinka mora imati minimum 6 karaktera.' ) );
-    }
-    // adding ability to set maximum allowed password chars -- uncomment the following two (2) lines to enable that
-    elseif (strlen($_POST['password_1']) > 64 )
-        $errors->add( 'woocommerce_password_error', __( 'Lozinka ne može imati više od 64 karaktera.' ) );
+add_action('validate_password_reset', 'gf_validate_password_reset', 10, 2);
+function gf_validate_password_reset($errors, $user)
+{
+    if (strlen($_POST['password_1']) < 6) {
+        $errors->add('woocommerce_password_error', __('Lozinka mora imati minimum 6 karaktera.'));
+    } // adding ability to set maximum allowed password chars -- uncomment the following two (2) lines to enable that
+    elseif (strlen($_POST['password_1']) > 64)
+        $errors->add('woocommerce_password_error', __('Lozinka ne može imati više od 64 karaktera.'));
     return $errors;
 }
 
@@ -130,6 +112,7 @@ function action_woocommerce_register_form()
     </div>
     <?php
 }
+
 add_action('woocommerce_register_form', 'action_woocommerce_register_form', 20, 10);
 
 //Custom addd to cart message
@@ -154,7 +137,7 @@ function gf_custom_add_to_cart_message($message)
 }
 
 
-add_filter('wp_authenticate_user', 'gf_check_if_user_is_migrated', 10, 2);
+//add_filter('wp_authenticate_user', 'gf_check_if_user_is_migrated', 10, 2);
 
 
 //function remove_country_field_billing($fields)
