398566b8342bb611456a1f8e191cce884eb71b1b
djavolak <djavolak@mail.ru>
2018-10-23 06:57:13 +0200

    bugfixig

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 //ini_set('upload_max_size', '128M');
 //ini_set('post_max_size', '128M');
-ini_set('max_execution_time', '120');
+//ini_set('max_execution_time', '80');
+ini_set('max_execution_time', '1200');
 
 require(__DIR__ . DIRECTORY_SEPARATOR . "user.functions.php");
 require(__DIR__ . DIRECTORY_SEPARATOR . "/search.functions.php");
