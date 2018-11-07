f7e30be59f1277ae04f816a3bffdc96d155a516f
djavolak <djavolak@mail.ru>
2018-11-07 19:52:25 +0100

    bugfixing

--- back-ajax.php
+++ back-ajax.php
@@ -42,7 +42,7 @@ if (isset($_GET['action'])) {
         case 'dailyExpressCsv': // wc-spz-slanje
             $arg = array('orderby' => 'date', 'status' => ['spz-pakovanje', 'spz-slanje'], 'posts_per_page' => '500');
             $orders = WC_get_orders($arg);
-            createDailyExport1($orders);
+            createDailyExport($orders);
 
             break;
 
@@ -62,105 +62,6 @@ if (isset($_GET['action'])) {
             break;
 
     }
-}
-
-function createDailyExport1($orders) {
-//    ini_set('display_errors', 1);
-    $adresnicaFields = [
-        'ReferenceID','SBranchID','SName','SAddress','STownID','STown','SCName','SCPhone','PuBranchID','PuName',
-        'PuAddress','PuTownID','PuTown','PuCName','PuCPhone','RBranchID','RName','RAddress','RTownID','RTown','RCName',
-        'RCPhone','DlTypeID','PaymentBy','PaymentType','BuyOut','Value','Mass','ReturnDoc','SMS_Sender','Packages','Note', 'Content'
-    ];
-
-    $adresnicaPath = ABSPATH . '/wp-content/uploads/daily.csv';
-    $tmpFile = fopen($adresnicaPath, 'wa');
-
-    //insert csv header
-    if (!fputcsv($tmpFile, $adresnicaFields, '|')) {
-        throw new Exception('Doslo je do greske prilikom generisanja csv izvestaja.');
-    }
-
-    /* @var \WC_Order $order */
-    foreach ($orders as $order) {
-        $billingName = $order->get_billing_first_name() .' '. $order->get_billing_last_name();
-        if ($order->get_shipping_address_1() !== ''):
-            $shippingName = $order->get_shipping_first_name() .' '. $order->get_shipping_last_name();
-            $shippingAddress = $order->get_shipping_address_1();
-            $shippingZip = $order->get_shipping_postcode();
-            $shippingCity = $order->get_shipping_city();
-        else:
-            $shippingName = $order->get_billing_first_name() .' '. $order->get_billing_last_name();
-            $shippingAddress = $order->get_billing_address_1();
-            $shippingZip = $order->get_billing_postcode();
-            $shippingCity = $order->get_billing_city();
-        endif;
-        $otkupnina = 0;
-        if ($order->get_payment_method_title() == 'Pouzećem') {
-            $otkupnina = number_format($order->get_total(), 0, '', '');
-            $otkupnina = $otkupnina * 100;
-        }
-        $weight = 0;
-        $category = '';
-        /* @var WC_Order_Item_Product $item */
-        foreach ($order->get_items() as $item) {
-            $weight += $item->get_product()->get_weight();
-            if (isset($item->get_product()->get_category_ids()[0])) {
-                $cat = get_term_by('id', $item->get_product()->get_category_ids()[0], 'product_cat');
-                $category = $cat->name;
-            }
-        }
-
-        $csvArray = array(
-            'gpoid' => $order->get_order_number(),
-
-            'ID nalogodavca' => 'UK17357',
-            'Naziv nalogodavca' => 'Non Stop Shop​ d.o.o.',
-            'Adresa nalogodavca' => 'Žorža Klemansoa 19',
-            'ID naselja nalogodavca' => 11000,
-            'Naziv naselja/mesta nalogodavca' => 'Beograd',
-            'Kontakt osoba nalogodavca' => 'NonStopShop',
-            'Kontakt telefon nalogodavca' => '011/3334773',
-
-            'ID posaljioca' => 'UK17357',
-            'Naziv posaljioca' => 'Non Stop Shop​ d.o.o.',
-            'Adresa mesta preuzimanja' => 'Žorža Klemansoa 19',
-            'ID naselja mesta preuzimanja' => 11000,
-            'Naziv naselja/mesta preuzimanja' => 'Beograd',
-            'Kontakt osoba mesta preuzimanja' => 'NonStopShop',
-            'Kontakt telefon mesta preuzimanja' => '011/3334773',
-
-            'ID primaoca' => '',
-            'Naziv primaoca' => $shippingName,
-            'Adresa primaoca' => $shippingAddress,
-            'ID naselja primaoca' => $shippingZip,
-            'Naziv naselja/mesta primaoca' => $shippingCity,
-            'Kontakt osoba primaoca' => $billingName,
-            'Kontakt telefon primaoca' => $order->get_billing_phone(),
-
-            'Tip isporuke' => 2,
-            'Ko plaća' => 1,
-            'Način plaćanja' => 2,
-            'Otkupnina' => $otkupnina,
-            'Vrednost' => 0,
-            'Masa' => $weight * 1000,
-            'Povratna dokumentacija' => '',
-            'SMS pošiljaoca' => '',
-            'Paketi' => 'SS'.preg_replace('/2018/', '', str_replace('-', '', $order->get_order_number()), 1), // i.e. 2018 -> '' , 2012 -> ''
-            'Napomena' => '',
-            'Content' => $category,
-        );
-
-        if (!fputcsv($tmpFile, $csvArray, '|')) {
-            die('eerrorr');
-        }
-        $order->update_status('poslato');
-    }
-
-    header('Content-Disposition: attachment; filename="adresnica-' . date('dmy') . '.csv' . '"');
-    header("Content-Transfer-Encoding: binary");
-    header('Expires: 0');
-    header('Pragma: no-cache');
-    echo file_get_contents($adresnicaPath);
 }
 
 function createJitexItemExport() {
@@ -390,12 +291,12 @@ function printPreorder(WC_Order $order) {
     require (__DIR__ . '/templates/orders/printPredracun.phtml');
     $html = ob_get_clean();
 
-    echo $html;
-//
-    $html2pdf = new \Spipu\Html2Pdf\Html2Pdf();
-    $html2pdf->writeHTML($html);
-    $name = 'Racun-'.$order->get_order_number().'.pdf';
-    $html2pdf->output($name, 'D');
+//    echo $html;
+
+//    $html2pdf = new \Spipu\Html2Pdf\Html2Pdf();
+//    $html2pdf->writeHTML($html);
+//    $name = 'Racun-'.$order->get_order_number().'.pdf';
+//    $html2pdf->output($name, 'D');
 }
 
 
