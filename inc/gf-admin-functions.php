25c5fbaebafc7bbe22a286564a31edab397e17e3
vjank <v.jankovic992@gmail.com>
2021-07-11 20:33:05 +0200

    fix(orderanalytics): saving product now redirects to new order page

--- inc/gf-admin-functions.php
+++ inc/gf-admin-functions.php
@@ -138,7 +138,7 @@ function redirect_page()
 {
     switch (get_post_type()) {
         case "shop_order":
-            $url = admin_url() . 'edit.php?post_type=shop_order';
+            $url = admin_url() . 'admin.php?page=order-analytics';
             wp_redirect($url);
             exit;
     }
