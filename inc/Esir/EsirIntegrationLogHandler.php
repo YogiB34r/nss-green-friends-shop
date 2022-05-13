1d91185f156a7f6c8bd3bc9e85c328d4c60b233a
v.jankovic992@gmail.com <Shingetsutan@qq11>
2022-05-13 11:44:08 +0200

    fix: fixing log for esir response

--- inc/Esir/EsirIntegrationLogHandler.php
+++ inc/Esir/EsirIntegrationLogHandler.php
@@ -7,20 +7,24 @@ class EsirIntegrationLogHandler
     public static $statusCode = [
         0 => 'waitingFiskalization',
         1 => 'fiskalizationSuccess',
-        2 => 'fistalizationFailed',
+        2 => 'fiskalizationFailed',
     ];
+
+    /**
+     *
+     * @param int $orderId
+     * @param string $response
+     * @return void
+     */
     public static function saveDropoxResponse(int $orderId, string $response)
     {
         global $wpdb;
-        $wpdb->insert('esir_log', array( 'orderId' => $orderId, 'dropboxResponse' => $response, 'status' => 0));
+        $wpdb->insert('esir_log', ['orderId' => $orderId, 'dropboxResponse' => $response, 'status' => 0]);
     }
 
     public static function saveEsirResponse(int $orderId, string $response, int $status = 2)
     {
         global $wpdb;
-        $wpdb->insert( 'esir_log', array( 'orderId' => $orderId, 'esirResponse' => $response, 'status' => $status ?? 2));
+        $wpdb->update('esir_log', ['esirResponse' => $response, 'status' => $status], ['orderId' => $orderId]);
     }
-
-
-
 }
